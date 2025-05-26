8ca2608fb296f89365cf5fc499f599b022c5261e
Candela94 <candelafsg@alumnos.cei.es>
2025-05-26 15:14:09 +0200

    incio2

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -5,11 +5,43 @@ import { Header } from "../../components/header/Header";
 import './inicio.css'
 
 import { BsChevronRight } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
+
+const imagenes = [
+
+    '/img/H1.jpg',
+    '/img/H2.jpg',
+    '/img/H3.jpg',
+    '/img/H4.jpg'
+
+
+]
 
 
 
 const Inicio = () => {
+
+    const [indice, setIndice] = useState(0)
+    const [primeraVez, setPrimeraVez] = useState(true)
+
+    useEffect(() => {
+
+
+        const intervalo = setInterval(() => {
+
+            setPrimeraVez(false);
+            setIndice((prev) => (prev + 1) % imagenes.length);
+
+        }, 500);
+
+        return () => clearInterval(intervalo)
+
+
+
+    }, [])
+
+
 
 
     return (
@@ -18,15 +50,17 @@ const Inicio = () => {
 
             <main className="Main-inicio">
 
-                
-                    <img src="/img/H1.jpg" alt="portada" className="Main-img" />
-           
+
+                <img
+
+
+                    src={imagenes[indice]} alt={`imagen-${indice}`} className={`Main-img ${primeraVez ? 'slideDown' : 'fade'}`} />
 
 
                 <div className="Main-texto">
 
                     <img src="/img/mode-logo.png" alt="logo" className="Logo" />
-                    <NavLink to='/home'><BsChevronRight className="Icono"/></NavLink>
+                    <NavLink to='/home'><BsChevronRight className="Icono" /></NavLink>
 
 
                 </div>
